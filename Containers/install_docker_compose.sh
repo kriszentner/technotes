33f11b83940efead28c4da16dee1640406a78eef
Kris Zentner <krisz@microsoft.com>
2018-08-02 12:44:12 -0700

    futureproof the script

--- Containers/install_docker_compose.sh
+++ Containers/install_docker_compose.sh
@@ -1,2 +1,3 @@
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#!/bin/bash
+sudo curl -L $(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url | cut -d '"' -f 4|grep "$(uname -s)-$(uname -m)$") -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
